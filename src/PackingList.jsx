97a4de7d6afcfb3cc5fcf03ed2ea1be33db69d2b
Christian Roy <64118768+constRoy24@users.noreply.github.com>
2023-09-06 10:05:32 +0800

    Sorting Items

--- src/PackingList.jsx
+++ src/PackingList.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Item from "./Item";
 
 const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
+  const [sortby, setSortby] = useState("input");
+  let sortedItems;
+  if (sortby === "input") sortedItems = items;
+  if (sortby === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortby === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <>
       <div className="list">
         <ul>
-          {items.map((item) => (
+          {sortedItems.map((item) => (
             <Item
               onDeleteItem={onDeleteItem}
               onToggleItem={onToggleItem}
@@ -14,6 +27,13 @@ const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
             />
           ))}
         </ul>
+        <div className="actions">
+          <select value={sortby} onChange={(e) => setSortby(e.target.value)}>
+            <option value="input">Sort by input order</option>
+            <option value="description">Sort by description</option>
+            <option value="packed">Sort by packed status</option>
+          </select>
+        </div>
       </div>
     </>
   );
